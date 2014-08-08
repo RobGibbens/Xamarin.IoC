d6dc7dbd713933909f81fe8ad005bc5ac5061947
Rob Gibbens <RobGibbens@gmail.com>
2014-08-08 16:49:04 -0400

    Ninject

---
+++ NinjectDemo/NinjectDemo.iOS/NinjectDemoViewController.cs
@@ -0,0 +1,33 @@
+using System;
+using MonoTouch.UIKit;
+using Ninject;
+using IoCDemo.Core;
+
+namespace NinjectDemo.iOS
+{
+	partial class NinjectDemoViewController : UIViewController
+	{
+		public NinjectDemoViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			var viewModel = App.Container.Get<MainViewModel> ();
+
+			var platformName = viewModel.PlatformName;
+			var container = viewModel.ContainerName;
+			var userName = viewModel.UserName;
+			var password = viewModel.Password;
+
+			Console.WriteLine ("Platform:{0} Container:{1} UserName:{2} Password:{3}", platformName, container, userName, password);
+
+			platformLabel.Text = "Platform : " + platformName;
+			containerLabel.Text = "Container : " + container;
+			userNameLabel.Text = "UserName : " + userName;
+			passwordLabel.Text = "Password : " + password;
+		}
+	}
+}
